f8447656055d468c29b52ce2d407b34c85fc02ff
WillianRRP <willianrenato754@gmail.com>
2025-05-05 14:01:46 -0300

    Avançando no uso do Cypress - invoke

--- cypress/e2e/privacyPolicy.cy.js
+++ cypress/e2e/privacyPolicy.cy.js
@@ -1,9 +1,10 @@
 /// <reference types="cypress" />
-
 describe('Central de Atendimento ao Cliente TAT - Política de Privacidade', () => {
-    beforeEach(() => {
-      cy.visit('src/privacy.html')
-    })
+  beforeEach(() => {
+    cy.visit('src/privacy.html')
+  })
+Cypress._.times(3, () =>{
+ 
     
 it('descrição do teste', () => {
   cy.contains('CAC TAT - Política de Privacidade').should('be.visible')
@@ -11,5 +12,6 @@ it('descrição do teste', () => {
   cy.contains('p', 'Utilzamos as tecnologias HTML, CSS e JavaScript, para simular uma aplicação real.').should('be.visible')
   cy.contains('p', 'No entanto, a aplicação é um exemplo, sem qualquer persistência de dados, e usada para fins de ensino.').should('be.visible')
   cy.contains('p', 'Talking About Testing').should('be.visible')
+})
 })
 })
